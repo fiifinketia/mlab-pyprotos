f23eb01035668bd07a4b35a0331dc389b1b62216
disal <disal@admin.git>
2024-07-03 16:51:32 +0000

    update task request

--- src/mlab_pyprotos/runner_pb2_grpc.py
+++ src/mlab_pyprotos/runner_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import mlab_pyprotos.runner_pb2 as runner__pb2
+import runner_pb2 as runner__pb2
 
 GRPC_GENERATED_VERSION = '1.64.1'
 GRPC_VERSION = grpc.__version__
