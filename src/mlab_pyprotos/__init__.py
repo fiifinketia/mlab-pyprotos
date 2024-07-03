ae3e34e9d7c86d14f4dc32a6f02659b699f1a7fa
disal <disal@admin.git>
2024-07-03 10:49:32 +0000

    init

---
+++ src/mlab_pyprotos/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    runner_pb2,
+    runner_pb2_grpc,
+    results_pb2,
+    results_pb2_grpc,
+)
+
+__all__ = [
+    "runner_pb2",
+    "runner_pb2_grpc",
+    "results_pb2",
+    "results_pb2_grpc",
+]
